4af1da578df04a324ee9bb16cefac428a8a426ab
justin3222 <justinmarlosibonga@gmail.com>
2025-10-09 19:55:58 +0800

    added feat

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -40,7 +40,8 @@ export default function Hero() {
                 width={1920}
                 height={1080}
                 priority
-                className="w-full h-auto"
+                className="w-full h-auto select-none"
+                draggable={false}
               />
             </div>
           </div>
